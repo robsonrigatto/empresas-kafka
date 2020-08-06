7ba95063b4249eaaf9fbec6fc209a7fb0faf4253
robsonrigatto <rigatto.robson@gmail.com>
2020-08-06 13:46:48 -0300

    exporting to csv

--- empresa-capital-persistence-service/src/main/java/br/com/rr/mastertech/empresa/capital/persistence/consumer/ReceitaConsumer.java
+++ empresa-capital-persistence-service/src/main/java/br/com/rr/mastertech/empresa/capital/persistence/consumer/ReceitaConsumer.java
@@ -8,8 +8,21 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 @Component
 public class ReceitaConsumer {
+
+    @Value("${receita.file.root.path}")
+    private String rootPath;
 
     @Autowired
     private KafkaTemplate<String, CapitalValidadoDTO> producer;
@@ -18,8 +31,14 @@ public class ReceitaConsumer {
     private String topic;
 
     @KafkaListener(topics = "${empresa.persistence.topic}", groupId = "grupo-do-robson")
-    public void consume(@Payload CapitalValidadoDTO receitaDTO) {
+    public void consume(@Payload CapitalValidadoDTO receitaDTO) throws IOException {
         System.out.println("persistindo cnpj: " + receitaDTO.getCnpj());
+
+        Path path = Paths.get(rootPath, "receita.csv");
+
+        List<String> lines = Collections.singletonList(String.format("%s,%s",
+                receitaDTO.getCnpj(), receitaDTO.getCapitalValido() ? "Sim" : "Não"));
+        Files.write(path, lines, StandardCharsets.UTF_8, new StandardOpenOption[]{StandardOpenOption.CREATE, StandardOpenOption.APPEND});
     }
 }
 
